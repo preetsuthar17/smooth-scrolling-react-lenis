d2ba603314a035726d8bad553d0aad17c9eb0590
Preet Suthar <preetsutharxd@gmail.com>
2024-03-13 19:55:10 +0530

    Add Files 🚀

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
 
+import ReactLenis from "@studio-freight/react-lenis";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ReactLenis
+      root
+      options={{
+        duration: 10.5,
+        SmoothScroll: true,
+        smoothWheel: true,
+        syncTouch: true,
+        smoothTouch: true,
+      }}
+    >
+      <Component {...pageProps} />;
+    </ReactLenis>
+  );
 }
